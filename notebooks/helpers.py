9d4a64b213d0784240ef2b267275b871c903c4a1
mbruns91 <m.bruns@mpie.de>
2025-05-15 12:40:46 +0000

    major updates

---
+++ notebooks/helpers.py
@@ -0,0 +1,126 @@
+from types import SimpleNamespace
+import ast
+import pandas as pd
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+from SPARQLWrapper import SPARQLWrapper
+import requests
+import re
+
+def canonify_string(name):
+    """
+    Remove all invalid characters for python identifiers (everything not a letter, number or underscores) 
+    """
+    return re.sub(r'[^a-zA-Z0-9_]', '_', name)
+
+class RecursiveNamespace(SimpleNamespace):
+    @staticmethod
+    def map_entry(entry):
+        if isinstance(entry, dict):
+            return RecursiveNamespace(**{
+                canonify_string(k): v for k, v in entry.items()
+            })
+        return entry
+
+    def __init__(self, **kwargs):
+        new_kwargs = {canonify_string(key): val for key, val in kwargs.items()}
+        super().__init__(**new_kwargs)
+        for key, val in new_kwargs.items():
+            if isinstance(val, dict):
+                setattr(self, key, RecursiveNamespace(**val))
+            elif isinstance(val, list):
+                setattr(self, key, [self.map_entry(v) for v in val])
+
+def recursive_iter(obj):
+    for key, value in vars(obj).items():
+        yield key, value  # recent level
+        # Recursion for nested objects
+        if isinstance(value, RecursiveNamespace):
+            yield from recursive_iter(value)
+        # handle lists which already contain RecursiveNamespace objects
+        elif isinstance(value, list):
+            for item in value:
+                if isinstance(item, RecursiveNamespace):
+                    yield from recursive_iter(item)
+
+def rectify_endpoints(endpoint):
+    ep_1 = endpoint.replace(":None/api/jena", "/api/v1/jena")
+    ep_2 = ep_1.replace(":443/api/jena", "/api/v1/jena")
+    return ast.literal_eval(ep_2)
+
+def make_dataframe(result, columns):
+    liste = []
+    for r in result['results']['bindings']:
+        row = []
+        for k in r.keys():
+            row.append(r[k]['value'])
+        liste.append(row)
+    df = pd.DataFrame(liste, columns = columns)
+    return df
+
+def send_query(endpoint: str | None = None,
+               token: str | None = None,
+               query: str | None = None,
+               columns: list[str] | None = None,
+               print_to_screen: bool = True
+              ) -> pd.DataFrame:
+    """
+    Send a SPARQL query to a provided endpoint.
+    Convert the result to a pandas dataframe and apply provided column headers.
+    Optionally print some context to screen.
+    Return the dataframe.
+
+    Args:
+
+    Returns:
+    """
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setReturnFormat('json')
+    sparql.addCustomHttpHeader("Authorization", f'Bearer {token}')
+    sparql.setQuery(query)
+    result = sparql.queryAndConvert()
+    result_df = make_dataframe(result, columns)
+    if print_to_screen:
+        print(f'Sending query to "{endpoint}". Result:')
+        print(result_df)
+        print("")
+    return result_df
+
+def query_instance(name: str,
+                   address: str,
+                   token: str,
+                   datasets: list[str] = [""],
+                   query: str | None = None,
+                   columns: list[str] | None = None,
+                   print_to_screen: bool = True
+                  ):
+    headers = {"Authorization": f"Bearer {token}"}
+    endpoints = rectify_endpoints(requests.get(f'{address}/api/v1/endpoints', headers=headers).content.decode())
+    
+    results = {}
+    name = canonify_string(name)
+    if name not in results:
+        results[name] = {}
+    for ep in endpoints:
+        if datasets is None or datasets == [""] or any(substring in ep for substring in datasets): # only query endpoints whose address contains any of the dataset names we passed
+            dataset_name = ep.split("/")[-2] # is this robust enough?
+            #print(dataset_name)
+            if dataset_name not in results[name]:
+                results[name][dataset_name] = {}
+            data = send_query(endpoint=ep, token=token, query=query, columns=columns, print_to_screen=print_to_screen)
+            results[name][dataset_name]["endpoint"] = ep
+            results[name][dataset_name]["query"] = query
+            results[name][dataset_name]["result"] = data
+
+    return results
+
+def federated_query(partners, datasets, query, columns, print_to_screen):
+    for key in partners.__dict__:
+        results = {}
+        if key not in results:
+            results[key] = {}
+        name = getattr(partners, key).ontodocker.name
+        address = getattr(partners, key).ontodocker.address
+        token = getattr(partners, key).ontodocker.token
+        results[key] = query_instance(name, address, token, datasets, query, columns, print_to_screen)
+    return results
---
+++ notebooks/queries.py
@@ -0,0 +1,252 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class SparqlQuery:
+    """Container for SPARQL queries and column headers corresponding to the expected result."""
+    query: str = ""
+    columns: list[str] = field(default_factory=list)
+
+class QueryCollection:
+    """
+    Central class for all queries and the corresponding column headers.
+    These queries are arbitrarily selected to fir the needs of hte pmd-demonstrator, specifically on the pmd-mesh.
+    They mostly work only for pmdco v2.0.7 and the tto (tensile test application ontology).
+    """
+
+    @staticmethod
+    def material_designation():
+        """
+        Define a query prompting for the value of materialDesignation.
+        Return the query and a list of column headers corresponding to the expected result.
+        """
+        query = """
+        PREFIX pmd: <https://w3id.org/pmd/co/>
+        SELECT distinct ?p ?matDesVal
+        WHERE {
+            ?s a pmd:TestPiece .
+            ?p pmd:input ?s .
+            ?p pmd:characteristic ?matDes .
+            ?matDes a pmd:materialDesignation .
+            ?matDes pmd:value ?matDesVal .
+        }
+        ORDER BY ?p
+        """
+        columns = ["URI", "materialDesignation"]
+        return SparqlQuery(query, columns)
+
+    @staticmethod
+    def process_type():
+        """
+        Define a query prompting for the value of processType.
+        Return the query and a list of column headers corresponding to the expected result.
+        """
+        query = """
+        PREFIX pmd: <https://w3id.org/pmd/co/>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        SELECT distinct ?p ?type
+        WHERE {
+            ?p a ?type .
+            ?matDes a pmd:materialDesignation .
+            ?matDes pmd:value "S355"^^xsd:string .
+            ?p pmd:characteristic ?matDes .
+        }
+        ORDER BY ?p
+        """
+        columns = ["URI", "Process type"]
+        return SparqlQuery(query, columns)
+
+    @staticmethod
+    def orientation():
+        """
+        Define a query prompting for the orientation in which a specimen
+        was cut from the raw material relative to the rolling direction.
+        Return the query and a list of column headers corresponding to the expected result.
+        """
+        query = """
+        PREFIX pmd: <https://w3id.org/pmd/co/>
+        SELECT distinct ?p ?rollingDirection
+        WHERE {
+            ?s a pmd:TestPiece .
+            ?p a pmd:TensileTest .
+            ?p pmd:input ?s .
+            ?p pmd:characteristic ?characteristic .
+            ?characteristic a pmd:MaterialRelated .
+            ?characteristic pmd:value ?rollingDirection .
+        }
+        ORDER BY ?p
+        """
+        columns = ["URI", "cut orientation"]
+        return SparqlQuery(query, columns)
+
+    @staticmethod
+    def standard():
+        """
+        Define a query prompting for the applied standards/norms during the test.
+        Return the query and a list of column headers corresponding to the expected result.
+        """
+        query = """
+        PREFIX pmd: <https://w3id.org/pmd/co/>
+        SELECT distinct ?p ?extensometerNameVal ?extensometerStandardVal
+        WHERE {
+            ?s a pmd:TestPiece .
+            ?p a pmd:TensileTest .
+            ?p pmd:input ?s .
+            ?p pmd:characteristic ?metadata .
+            ?extensometerName a pmd:NodeName .
+            ?extensometerName pmd:value ?extensometerNameVal .
+            ?extensometerStandard a pmd:Norm .
+            ?extensometerStandard pmd:value ?extensometerStandardVal .
+            FILTER (?extensometerName!=<https://w3id.org/pmd/ao/tte/_machineName>)
+            FILTER (?extensometerStandard=<https://w3id.org/pmd/ao/tte/_extensometerStandard>)
+        }
+        ORDER BY ?p
+        """
+        columns = ["URI", "Extensiometer model", "Standard"]
+        return SparqlQuery(query, columns)
+
+    @staticmethod
+    def extensiometer():
+        """
+        Define a query prompting for the tensile test machines model name.
+        Return the query and a list of column headers corresponding to the expected result.
+        """
+        query = """
+        PREFIX pmd: <https://w3id.org/pmd/co/>
+        SELECT distinct ?p ?extensometerNameVal
+        WHERE {
+        ?s a pmd:TestPiece .
+        ?p a pmd:TensileTest .
+        ?p pmd:input ?s .
+        ?p pmd:characteristic ?metadata .
+        ?extensometerName a pmd:NodeName .
+        ?extensometerName pmd:value ?extensometerNameVal .
+        FILTER (?extensometerName!=<https://w3id.org/pmd/ao/tte/_machineName>)
+        } ORDER BY ?p
+        """
+        columns = ["URI", "Extensiometer model"]
+        return SparqlQuery(query, columns)
+
+    @staticmethod
+    def specimen_id():
+        """
+        Define a query prompting for the specimen's ID.
+        Return the query and a list of column headers corresponding to the expected result.
+        """
+        query = """
+        PREFIX pmd: <https://w3id.org/pmd/co/>
+        SELECT distinct ?p ?s
+        WHERE {
+        ?s a pmd:TestPiece .
+        ?p a pmd:TensileTest .
+        ?p pmd:input ?s .
+        } ORDER BY ?p
+        """
+        columns = ["URI", "Specimen ID"]
+        return SparqlQuery(query, columns)
+
+    @staticmethod
+    def csv_url():
+        """
+        Define a query prompting for the URL pointing to the csv file containing strass-strain data.
+        Return the query and a list of column headers corresponding to the expected result.
+        """
+        query = """
+        PREFIX base: <https://w3id.org/pmd/co/>
+        PREFIX csvw: <http://www.w3.org/ns/csvw#>
+        SELECT ?p ?url
+        WHERE {
+            ?p a base:TensileTest .
+            ?p base:characteristic ?dataset .
+            ?dataset a base:Dataset .
+            ?dataset base:resource ?table .
+            ?table a csvw:Table .
+            ?table csvw:url ?url .
+        }
+        ORDER BY ?p
+        """
+        columns = ["URI", "URL"]
+        return SparqlQuery(query, columns)
+
+    @staticmethod
+    def primary_data(uri: str | None = None):
+        """
+        Define a query prompting for all PrimaryData values accossiated with a certain process, hinted via its URI.
+        Return the query and a list of column headers corresponding to the expected result.
+
+        Args:
+            uri (str | None): URI used to identify the process of question
+        Returns:
+            ...
+        """
+        query = """
+        PREFIX pmd: <https://w3id.org/pmd/co/>
+        SELECT distinct ?p ?o ?v ?u
+        WHERE {{
+        ?s a pmd:TestPiece .
+        ?p a pmd:TensileTest .
+        ?p pmd:input ?s .
+        ?p pmd:characteristic ?o .
+        ?o a pmd:PrimaryData .
+        ?o pmd:value ?v .
+        ?o pmd:unit ?u .
+        FILTER regex(str(?p), "{uri:s}")
+        }} ORDER BY ?p
+        """.format(uri=uri)
+        columns = ['URI', 'Quantity', 'value', 'unit']
+        return SparqlQuery(query, columns)
+    
+    @staticmethod
+    def secondary_data(uri: str | None = None):
+        """
+        Define a query prompting for all SecondaryData values accossiated with a certain process, hinted via its URI.
+        Return the query and a list of column headers corresponding to the expected result.
+        
+        Args:
+            uri (str | None): URI used to identify the process of question
+        Returns:
+            ...
+        """
+        query = """
+        PREFIX pmd: <https://w3id.org/pmd/co/>
+        SELECT distinct ?p ?o ?v ?u
+        WHERE {{
+        ?s a pmd:TestPiece .
+        ?p a pmd:TensileTest .
+        ?p pmd:input ?s .
+        ?p pmd:characteristic ?o .
+        ?o a pmd:SecondaryData .
+        ?o pmd:value ?v .
+        ?o pmd:unit ?u .
+        FILTER regex(str(?p), "{uri:s}")
+        }} ORDER BY ?p
+        """.format(uri=uri)
+        columns = ['URI', 'Quantity', 'value', 'unit']
+        return SparqlQuery(query, columns)
+
+    @staticmethod
+    def metadata(uri: str | None = None):
+        """
+        Define a query prompting for all Metadata values accossiated with a certain process, hinted via its URI.
+        Return the query and a list of column headers corresponding to the expected result.
+        
+        Args:
+            uri (str | None): URI used to identify the process of question
+        Returns:
+            ...
+        """
+        query = """
+        PREFIX pmd: <https://w3id.org/pmd/co/>
+        SELECT distinct ?p ?o ?v ?u
+        WHERE {{
+        ?s a pmd:TestPiece .
+        ?p a pmd:TensileTest .
+        ?p pmd:input ?s .
+        ?p pmd:characteristic ?o .
+        ?o a pmd:Metadata .
+        ?o pmd:value ?v .
+        ?o pmd:unit ?u .
+        FILTER regex(str(?p), "{uri:s}")
+        }} ORDER BY ?p
+        """.format(uri=uri)
+        columns = ['URI', 'Quantity', 'value', 'unit']
+        return SparqlQuery(query, columns)
