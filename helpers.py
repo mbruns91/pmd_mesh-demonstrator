692c883ce3bebd519f715347cd5339bb61dded95
mbruns91 <m.bruns@mpie.de>
2025-06-13 09:05:41 +0000

    move helper definitions to a better place

--- helpers.py
+++ helpers.py
@@ -7,21 +7,29 @@ from SPARQLWrapper import SPARQLWrapper
 import requests
 import re
 
+def canonify_string(name):
+    """
+    Remove all invalid characters for python identifiers (everything not a letter, number or underscore) 
+    """
+    return re.sub(r'[^a-zA-Z0-9_]', '_', name)
+
 class RecursiveNamespace(SimpleNamespace):
     @staticmethod
     def map_entry(entry):
         if isinstance(entry, dict):
-            return RecursiveNamespace(**entry)
+            return RecursiveNamespace(**{
+                canonify_string(k): v for k, v in entry.items()
+            })
         return entry
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        for key, val in kwargs.items():
+        new_kwargs = {canonify_string(key): val for key, val in kwargs.items()}
+        super().__init__(**new_kwargs)
+        for key, val in new_kwargs.items():
             if isinstance(val, dict):
                 setattr(self, key, RecursiveNamespace(**val))
             elif isinstance(val, list):
                 setattr(self, key, [self.map_entry(v) for v in val])
-
 
 def recursive_iter(obj):
     for key, value in vars(obj).items():
@@ -90,11 +98,11 @@ def query_instance(name: str,
     endpoints = rectify_endpoints(requests.get(f'{address}/api/v1/endpoints', headers=headers).content.decode())
     
     results = {}
-    name = re.sub(r'[^a-zA-Z0-9_]', '_', name) # remove all invalid characters for python identifiers (everything not a letter, number or underscores) 
+    name = canonify_string(name)
     if name not in results:
         results[name] = {}
     for ep in endpoints:
-        if any(substring in ep for substring in datasets): # only query endpoints whose address contains any of the dataset names we passed
+        if datasets is None or datasets == [""] or any(substring in ep for substring in datasets): # only query endpoints whose address contains any of the dataset names we passed
             dataset_name = ep.split("/")[-2] # is this robust enough?
             #print(dataset_name)
             if dataset_name not in results[name]:
@@ -111,9 +119,8 @@ def federated_query(partners, datasets, query, columns, print_to_screen):
         results = {}
         if key not in results:
             results[key] = {}
-        for key in partners.__dict__:
-            name = getattr(partners, key).ontodocker.name
-            address = getattr(partners, key).ontodocker.address
-            token = getattr(partners, key).ontodocker.token
-            results[key] = query_instance(name, address, token, datasets, query, columns, print_to_screen)
+        name = getattr(partners, key).ontodocker.name
+        address = getattr(partners, key).ontodocker.address
+        token = getattr(partners, key).ontodocker.token
+        results[key] = query_instance(name, address, token, datasets, query, columns, print_to_screen)
     return results
